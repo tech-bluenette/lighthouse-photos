6c0b3e96fc684f68c3ccad66302dc2634f9fd529
Lindsey Miller <lindseyandmina@gmail.com>
2014-01-14 04:36:50 +0000

    slideshoe working

--- lighthouse.js
+++ lighthouse.js
@@ -1,33 +1,41 @@
-// 1. Wrap link in image tag
-  // 2. Show only one image at a time; 
-  // 3. rotate images using fadein/fadeout every couple of seconds
+
+  
+  var url = "https://api.instagram.com/v1/tags/lighthouse/media/recent?access_token=220041863.f59def8.6c6165821a5d4d2bb41bb31fd9db4f90&callback=?";
+  
 
 
-// CLIENT ID d403150a12f3461a88d3b6cf928b46e3
-// CLIENT SECRET 45b095aff0854d919cc7a0395f1ca2c2
-// WEBSITE URL https://github.com/tech-bluenette/lighthouse-photos
-// REDIRECT URI  https://github.com/tech-bluenette
+  var lighthousePhotoRetriever = function(data) {
+      var instagrams = data.data;
+      for(var i=0; i < instagrams.length; i++) {
+          var photo = instagrams[i]; 
+        $('#slideshow').append("<img src='"+photo.images.low_resolution.url+"'>");
+      }       
+  }
+
+
+function slideSwitch() {
+  var $active = $('#slideshow IMG.active');
+
+  if ( $active.length == 0 ) $active = $('#slideshow IMG:last');
+
+  var $next =  $active.next().length ? $active.next()
+      : $('#slideshow IMG:first');
+
+  $active.addClass('last-active');
+      
+  $next.css({opacity: 0.0})
+      .addClass('active')
+      .animate({opacity: 1.0}, 1000, function() {
+          $active.removeClass('active last-active');
+      });
+}
 
 $(document).ready(function() {
-    
-    var url = "https://api.instagram.com/v1/tags/lighthouse/media/recent?access_token=220041863.f59def8.6c6165821a5d4d2bb41bb31fd9db4f90&callback=?";
-    
+    $.getJSON(url, lighthousePhotoRetriever);    
 
-
-    var lighthousePhotoRetriever = function(data) {
-        var instagrams = data.data;
-        for(var i=0; i < instagrams.length; i++) {
-            var photo = instagrams[i]; 
-          $('#photos-list').append("<li><img src='"+photo.images.low_resolution.url+"'></li>");
-        }       
-    }
-    
-    
     $('#load').click(function() {
-        $.getJSON(url, lighthousePhotoRetriever);        
-    });
-
-
+        setInterval( "slideSwitch()", 5000 );
+    });    
 });
 
 
